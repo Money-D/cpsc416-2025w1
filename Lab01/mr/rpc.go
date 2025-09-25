d27d52ff57ed3ff5219297262ee927a6497f0f46
Money <money_sd@hotmail.com>
2025-09-25 12:27:54 -0700

    Add initial RPC args and reply structs

--- Lab01/mr/rpc.go
+++ Lab01/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -24,6 +26,34 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+type RegisterArgs struct{}
+
+type RegisterReply struct {
+	Wid int
+}
+
+type TaskRequestArgs struct {
+	Wid int
+}
+type TaskRequestReply struct {
+	Type              string   // "map", "reduce", "wait", "exit"
+	FileName          string   // only for map tasks
+	NReduce           int      // only for map tasks
+	ReduceId          int      // only for reduce tasks
+	IntermediateFiles []string // only for reduce tasks
+}
+
+type TaskCompleteArgs struct {
+	Wid int
+}
+type TaskCompleteReply struct {
+}
+
+type HeartbeatArgs struct {
+	Wid int
+}
+type HeartbeatReply struct {
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
